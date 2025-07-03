2bb23076b7b3138c619d45e976e3c0fe6d947aa3
Ben Mordue <benmordue@gmail.com>
2025-07-03 11:36:04 +0100

    fix: Prevent SPARQL injection and fix vCard import
    This commit addresses three issues:
    
    1.  **SPARQL Injection in Date Parameters:**
        The `list-events-in-range` function was vulnerable to SPARQL
        injection because it directly used the `start-date` and `end-date`
        parameters in the query string. This has been fixed by adding
        date validation.
    
    2.  **vCard Import with `wrap-json-body`:**
        The `wrap-json-body` middleware was incorrectly trying to parse
        `text/vcard` requests as JSON. This has been resolved by
        creating a `wrap-vcard-body` middleware that only parses JSON
        when the content type is `application/json`.
    
    3.  **SPARQL Injection in Full Name Parameter:**
        The `get-contact-by-name` function was vulnerable to SPARQL
        injection. This has been fixed by using a parameterized SPARQL
        query.

--- rwclj/src/rwclj/db.clj
+++ rwclj/src/rwclj/db.clj
@@ -1,42 +1,49 @@
 (ns my-clojure-project.db
   (:require [clojure.tools.logging :as log])
   (:import [org.apache.jena.tdb2 TDB2Factory]
-           [org.apache.jena.query QueryFactory QueryExecutionFactory]
+           [org.apache.jena.query QueryFactory QueryExecutionFactory QuerySolutionMap]
            [org.apache.jena.rdf.model Model RDFNode]))
 
 (defn get-dataset []
   (let [dataset-path (or (System/getenv "JENA_DB_PATH") "data/tdb2")]
     (TDB2Factory/connectDataset dataset-path)))
 
-(defn execute-sparql-select [query-string]
-  (let [dataset (get-dataset)]
-    (try
-      (.begin dataset)
-      (let [model (.getDefaultModel dataset)
-            query (QueryFactory/create query-string)
-            qexec (QueryExecutionFactory/create query model)
-            results (.execSelect qexec)
-            result-list (atom [])]
-        (while (.hasNext results)
-          (let [soln (.nextSolution results)
-                vars (.varNames soln)
-                row (reduce (fn [acc var]
-                              (let [node (.get soln var)]
-                                (assoc acc (keyword var)
-                                       (cond
-                                         (.isResource node) (.getURI node)
-                                         (.isLiteral node) (.getLexicalForm node)
-                                         :else (.toString node)))))
-                            {} vars)]
-            (swap! result-list conj row)))
-        (.close qexec)
-        @result-list)
-      (catch Exception e
-        (log/error e "Error executing SPARQL query")
-        [])
-      (finally
-        (.end dataset) ; Ensure dataset is ended in finally block
-        (.close dataset)))))
+(defn execute-sparql-select
+  ([query-string]
+   (execute-sparql-select query-string {}))
+  ([query-string params]
+   (let [dataset (get-dataset)]
+     (try
+       (.begin dataset)
+       (let [model (.getDefaultModel dataset)
+             query (QueryFactory/create query-string)
+             param-map (reduce-kv (fn [m k v]
+                                    (.add m (name k) v)
+                                    m)
+                                  (QuerySolutionMap.)
+                                  params)]
+         (with-open [qexec (QueryExecutionFactory/create query model param-map)]
+           (let [results (.execSelect qexec)
+                 result-list (atom [])]
+             (while (.hasNext results)
+               (let [soln (.nextSolution results)
+                     vars (.varNames soln)
+                     row (reduce (fn [acc var]
+                                   (let [node (.get soln var)]
+                                     (assoc acc (keyword var)
+                                            (cond
+                                              (.isResource node) (.getURI node)
+                                              (.isLiteral node) (.getLexicalForm node)
+                                              :else (.toString node)))))
+                                 {} vars)]
+                 (swap! result-list conj row)))
+             @result-list)))
+       (catch Exception e
+         (log/error e "Error executing SPARQL query")
+         [])
+       (finally
+         (.end dataset)
+         (.close dataset))))))
 
 (defn store-rdf-model! [^Model model]
   (let [dataset (get-dataset)
