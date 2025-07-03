46625a8e44c215989d886f9b4bc484400fcfe088
Ben Mordue <benmordue@gmail.com>
2025-07-03 12:26:20 +0100

    remove jena from shell.nix
    will be installed used clj deps

--- shell.nix
+++ shell.nix
@@ -1,31 +1,10 @@
 # shell.nix
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  jenaVersion = "4.10.0";
-  jenaUrl = "https://archive.apache.org/dist/jena/binaries/apache-jena-${jenaVersion}.tar.gz";
-  
-  jena = pkgs.stdenv.mkDerivation {
-    pname = "apache-jena";
-    version = jenaVersion;
-    
-    src = pkgs.fetchurl {
-      url = jenaUrl;
-      sha256 = "1qs7rfm133cip6vyrn6max4lwsfsni1przia739glnbnj63h4s8v";
-    };
-    
-    installPhase = ''
-      mkdir -p $out
-      cp -r * $out/
-      chmod +x $out/bin/*
-    '';
-  };
-
-in pkgs.mkShell {
+pkgs.mkShell {
   buildInputs = with pkgs; [
     clojure
     openjdk17
-    jena
     gemini-cli
   ];
   
@@ -33,7 +12,6 @@ in pkgs.mkShell {
     export PROJECT_ROOT=$(pwd)
     export DATA_DIR=$PROJECT_ROOT/data
     export MEDIA_DIR=$PROJECT_ROOT/media
-    export JENA_HOME=${jena}
     export PATH=$JENA_HOME/bin:$PATH
     
     # Create isolated directories
