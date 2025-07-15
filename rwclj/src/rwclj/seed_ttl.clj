517b6b78e447a3865e605190b94480a14a802976
Ben Mordue <benmordue@gmail.com>
2025-07-15 10:17:11 +0100

    gemini review feedback

--- rwclj/src/rwclj/seed_ttl.clj
+++ rwclj/src/rwclj/seed_ttl.clj
@@ -1,4 +1,4 @@
-(ns my-clojure-project.seed
+(ns rwclj.seed-ttl
   (:require [clojure.string :as str]))
 
 ;; Define sample data: a list of people
@@ -28,35 +28,33 @@
 (defn format-rdf-term [term]
   (cond
     (keyword? term) (str (namespace term) ":" (name term)) ;; Assumes keywords are qnames like :foaf/name
-    (and (string? term) (str/starts-with? term "http://")) (str "<" term ">") ;; Full URI
-    (and (string? term) (str/starts-with? term "mailto:")) (str "<" term ">") ;; mailto URI
-    (string? term) (str "\"" term "\"") ;; Plain literal
-    (number? term) (str term) ;; Number literal
-    :else (str "\"" term "\"^^<http://www.w3.org/2001/XMLSchema#string>"))) ;; Default to xsd:string
+    (and (string? term) (re-find #"^(https?|mailto):" term)) (str "<" term ">")
+    (string? term) (str "\"" (str/replace term #"\"" "\\\"") "\"")
+    (number? term) (str term)
+    :else (str "\"" (str/replace (str term) #"\"" "\\\"") "\"^^<http://www.w3.org/2001/XMLSchema#string>")))
 
-(defn resource-to-ttl [resource-map]
-  (let [subject (:uri resource-map)]
-    (if (nil? subject)
-      (throw (IllegalArgumentException. (str "Resource map missing :uri key: " resource-map))))
-    (->> (dissoc resource-map :uri) ; Remove :uri, process other key-value pairs as predicates and objects
-         (map (fn [[predicate object]]
-                (if (coll? object) ; Handle multiple values for the same predicate
-                  (str/join " ;\n    " (map #(str (format-rdf-term predicate) " " (format-rdf-term %) ) object))
-                  (str (format-rdf-term predicate) " " (format-rdf-term object)))))
-         (str/join " ;\n    ")
-         (str (format-rdf-term subject) " \n    ")
-         (str "\n.\n"))))
+  (defn resource-to-ttl [resource-map]
+    (let [subject (:uri resource-map)]
+      (if (nil? subject)
+        (throw (IllegalArgumentException. (str "Resource map missing :uri key: " resource-map))))
+      (->> (dissoc resource-map :uri) ; Remove :uri, process other key-value pairs as predicates and objects
+           (map (fn [[predicate object]]
+                  (if (coll? object)
+                    (str (format-rdf-term predicate) " " (str/join ", " (map format-rdf-term object)))
+                    (str (format-rdf-term predicate) " " (format-rdf-term object)))))
+           (str/join " ;\n    ")
+           (str (format-rdf-term subject) " \n    ")
+           (str "\n.\n"))))
 
 
-(defn generate-ttl-string [data]
-  (let [prefix-str (format-prefix-map prefixes)
-        triple-str (str/join "" (map resource-to-ttl data))]
-    (str prefix-str "\n\n" triple-str)))
+  (defn generate-ttl-string [data]
+    (let [prefix-str (format-prefix-map prefixes)
+          triple-str (str/join "" (map resource-to-ttl data))]
+      (str prefix-str "\n\n" triple-str)))
 
-(comment
-  ;; This is how you might use it later
-  (println (generate-ttl-string people-data))
-  )
+  (comment
+    ;; This is how you might use it later
+    (println (generate-ttl-string people-data)))
 
-(defn -main [& args]
-  (println (generate-ttl-string people-data)))
+  (defn -main [& args]
+    (println (generate-ttl-string people-data)))
