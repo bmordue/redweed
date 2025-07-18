d546ee37fe86a5e3450084fcea7f50b280c89065
Ben Mordue <BMordue@hymans.co.uk>
2025-07-18 16:54:10 +0100

    clj dir was confusing agents, no longer planning to use it

--- rwclj/bin/mutate.clj
+++
@@ -1,8 +0,0 @@
-  (ns mutate
-    (:require [mutant :as mutant]
-              [clojure.test :as clojure-test]))
-  
-  (binding [*out* *err*]
-    (mutant/run! ["src"]
-                 ["test"]
-                 clojure.test/run-tests))
--- rwclj/src/rwclj/core.clj
+++
@@ -1,7 +0,0 @@
-(ns rwclj.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
