5d4df4c065a1422f266603530ff6c073cb042b2e
Ben Mordue <BMordue@hymans.co.uk>
2025-07-18 09:42:43 +0100

    missing import

--- jweed/src/main/java/me/bmordue/redweed/service/EventService.java
+++ jweed/src/main/java/me/bmordue/redweed/service/EventService.java
@@ -3,6 +3,7 @@ package me.bmordue.redweed.service;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.exceptions.HttpStatusException;
 import jakarta.inject.Inject;
+import me.bmordue.redweed.exception.ICalParsingException;
 import me.bmordue.redweed.model.dto.IngestICalResponseDto;
 import me.bmordue.redweed.repository.EventRepository;
 import org.apache.jena.rdf.model.Model;
@@ -19,9 +20,9 @@ public class EventService {
         Model model;
         try {
             model = ICalToRdfConverter.convert(ics);
-    } catch (ICalParsingException e) { // Replace RuntimeException with the specific exception
-        throw new HttpStatusException(HttpStatus.BAD_REQUEST, "Could not import event");
-    }
+        } catch (ICalParsingException e) { // Replace RuntimeException with the specific exception
+            throw new HttpStatusException(HttpStatus.BAD_REQUEST, "Could not import event");
+        }
 
         eventRepository.save(model);
 
