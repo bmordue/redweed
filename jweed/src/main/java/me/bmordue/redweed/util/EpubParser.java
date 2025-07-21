ab61f0fa111df6e071841c98d9d59450b823c215
Ben Mordue <BMordue@hymans.co.uk>
2025-07-21 10:39:03 +0100

    fix swagger config

--- jweed/src/main/java/me/bmordue/redweed/util/EpubParser.java
+++ jweed/src/main/java/me/bmordue/redweed/util/EpubParser.java
@@ -1,15 +1,16 @@
 package me.bmordue.redweed.util;
 
 import me.bmordue.redweed.exception.EpubParserException;
-import nl.siegmann.epublib.domain.Book;
-import nl.siegmann.epublib.domain.Metadata;
-import nl.siegmann.epublib.epub.EpubReader;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,26 +23,68 @@ public class EpubParser {
 
     public static Map<String, String> parse(File file) {
         Map<String, String> metadata = new HashMap<>();
-        try (InputStream in = new FileInputStream(file)) {
-            EpubReader epubReader = new EpubReader();
-            Book book = epubReader.readEpub(in);
-            Metadata meta = book.getMetadata();
-            if (!meta.getTitles().isEmpty()) {
-                metadata.put("title", meta.getTitles().get(0));
+
+        try (FileInputStream fis = new FileInputStream(file);
+             ZipArchiveInputStream zip = new ZipArchiveInputStream(fis)) {
+
+            ZipArchiveEntry entry;
+            while ((entry = zip.getNextZipEntry()) != null) {
+                if (entry.getName().endsWith(".opf") || entry.getName().contains("content.opf")) {
+                    // Parse the OPF file for metadata
+                    parseOpfMetadata(zip, metadata);
+                    break;
+                }
             }
-            if (!meta.getAuthors().isEmpty()) {
-                metadata.put("creator", meta.getAuthors().get(0).getFirstname() + " " + meta.getAuthors().get(0).getLastname());
-            }
-            if (!meta.getPublishers().isEmpty()) {
-                metadata.put("publisher", meta.getPublishers().get(0));
-            }
-            if (!meta.getDates().isEmpty()) {
-                metadata.put("date", meta.getDates().get(0).getValue());
-            }
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new EpubParserException("Error reading EPUB file: " + file.getName(), e);
         }
+
         return metadata;
+    }
+
+    private static void parseOpfMetadata(InputStream opfStream, Map<String, String> metadata) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(opfStream);
+
+            Element root = doc.getDocumentElement();
+
+            // Parse Dublin Core metadata
+            NodeList titleNodes = root.getElementsByTagName("dc:title");
+            if (titleNodes.getLength() > 0) {
+                metadata.put("title", titleNodes.item(0).getTextContent().trim());
+            }
+
+            NodeList creatorNodes = root.getElementsByTagName("dc:creator");
+            if (creatorNodes.getLength() > 0) {
+                metadata.put("creator", creatorNodes.item(0).getTextContent().trim());
+            }
+
+            NodeList publisherNodes = root.getElementsByTagName("dc:publisher");
+            if (publisherNodes.getLength() > 0) {
+                metadata.put("publisher", publisherNodes.item(0).getTextContent().trim());
+            }
+
+            NodeList dateNodes = root.getElementsByTagName("dc:date");
+            if (dateNodes.getLength() > 0) {
+                metadata.put("date", dateNodes.item(0).getTextContent().trim());
+            }
+
+            NodeList identifierNodes = root.getElementsByTagName("dc:identifier");
+            if (identifierNodes.getLength() > 0) {
+                metadata.put("identifier", identifierNodes.item(0).getTextContent().trim());
+            }
+
+            NodeList languageNodes = root.getElementsByTagName("dc:language");
+            if (languageNodes.getLength() > 0) {
+                metadata.put("language", languageNodes.item(0).getTextContent().trim());
+            }
+
+        } catch (Exception e) {
+            throw new EpubParserException("Error parsing OPF metadata", e);
+        }
     }
 
     private static String getTagValue(String tag, Element element) {
