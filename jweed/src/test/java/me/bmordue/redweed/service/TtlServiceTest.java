671a75b0fbb3f75082784f23347f9bf4615a81a1
Ben Mordue <BMordue@hymans.co.uk>
2025-07-23 16:17:40 +0100

    review feedback: more robust test assertion in TtlServiceTest

--- jweed/src/test/java/me/bmordue/redweed/service/TtlServiceTest.java
+++ jweed/src/test/java/me/bmordue/redweed/service/TtlServiceTest.java
@@ -2,13 +2,15 @@ package me.bmordue.redweed.service;
 
 import me.bmordue.redweed.repository.RdfRepository;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,16 +26,30 @@ class TtlServiceTest {
     void ingestTtl() {
         // Given
         String ttl = """
-            @prefix : <http://example.org/> .
-            @prefix foaf: <http://xmlns.com/foaf/0.1/> .
-
-            :a foaf:name "Test" .
-            """;
+                @prefix : <http://example.org/> .
+                @prefix foaf: <http://xmlns.com/foaf/0.1/> .
+                
+                :a foaf:name "Test" .
+                """;
 
         // When
         ttlService.ingestTtl(ttl);
 
         // Then
-        verify(rdfRepository).save(any(Model.class));
+        ArgumentCaptor<Model> modelCaptor = ArgumentCaptor.forClass(Model.class);
+        verify(rdfRepository).save(modelCaptor.capture());
+
+        Model capturedModel = modelCaptor.getValue();
+
+        // Validate that the model contains the expected resource
+        Resource subject = capturedModel.getResource("http://example.org/a");
+        assertTrue(capturedModel.containsResource(subject));
+
+        // Validate that the foaf:name property exists with the correct value
+        assertTrue(capturedModel.contains(
+                subject,
+                capturedModel.getProperty("http://xmlns.com/foaf/0.1/name"),
+                capturedModel.createLiteral("Test")
+        ));
     }
 }
