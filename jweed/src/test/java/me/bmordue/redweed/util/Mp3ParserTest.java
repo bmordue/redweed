9f1a1228b034e07d1bb3736d71e65d92c07972c2
Ben Mordue <BMordue@hymans.co.uk>
2025-07-24 17:05:15 +0100

    couple more tests

--- jweed/src/test/java/me/bmordue/redweed/util/Mp3ParserTest.java
+++ jweed/src/test/java/me/bmordue/redweed/util/Mp3ParserTest.java
@@ -3,7 +3,9 @@ package me.bmordue.redweed.util;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class Mp3ParserTest {
@@ -12,5 +14,17 @@ class Mp3ParserTest {
     void testParse() {
         File file = new File(getClass().getClassLoader().getResource("test.mp3").getFile());
         assertThrows(RuntimeException.class, () -> Mp3Parser.parse(file));
+    }
+
+    @Test
+    void testParseSuccess() {
+        File file = new File(getClass().getClassLoader().getResource("test.mp3").getFile());
+        Map<String, String> metadata = Mp3Parser.parse(file);
+        // Replace these expected values with the actual metadata in your test.mp3
+        assertEquals("Test Title", metadata.get("title"));
+        assertEquals("Test Artist", metadata.get("artist"));
+        assertEquals("Test Album", metadata.get("album"));
+        assertEquals("1", metadata.get("track"));
+        assertEquals("Pop", metadata.get("genre"));
     }
 }
--- jweed/src/test/java/me/bmordue/redweed/util/Mp4ParserTest.java
+++ jweed/src/test/java/me/bmordue/redweed/util/Mp4ParserTest.java
@@ -14,4 +14,13 @@ class Mp4ParserTest {
         File file = new File(getClass().getClassLoader().getResource("test.mp4").getFile());
         assertThrows(RuntimeException.class, () -> Mp4Parser.parse(file));
     }
+
+    @Test
+    void testParseValidMp4GeneratesThumbnailMetadata() throws IOException {
+        File file = new File(getClass().getClassLoader().getResource("test.mp4").getFile());
+        var metadata = Mp4Parser.parse(file);
+        // Check that the metadata contains a thumbnail key and it is a File
+        assert(metadata.containsKey("thumbnail"));
+        assert(metadata.get("thumbnail") instanceof File);
+    }
 }
